71e2ecb7e3db5a61b1f8198df2fab3fe8180dddb
hoanghaibui <hoanghaibui2003@gmail.com>
2025-02-15 11:53:23 +0800

    Setup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-amber-300">
+        Hello word
+    </div>
+  )
+}
+
+export default App
